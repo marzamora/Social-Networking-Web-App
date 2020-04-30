cf504a0220e059b5be3559311e3330b6bd156ef1
Marcello Zamora <zamoramaj@gmail.com>
2020-04-30 15:26:51 -0400

    Added route for user profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,24 @@
 import { NgModule, Component } from "@angular/core";
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegistrationComponent } from './auth/registration/registration.component';
 import { HomeComponent } from './home/home.component';
 import { AuthGuard } from './auth/auth.guard';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const routes: Routes = [
     {
         path: 'login',
-        component: LoginComponent
+        component: LoginComponent,
+        // canDeActivate: TODO
     },
     {
         path: 'register',
         component: RegistrationComponent
+    },
+    {
+        path: 'profile',
+        component: UserProfileComponent
     },
     {
         path: '',
@@ -28,7 +34,7 @@ const routes: Routes = [
 
 @NgModule({
     // imports: [RouterModule.forRoot(routes, {enableTracing: true})],
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})], 
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
