dd9454c3480fdb314f44c9729220d1909fbadf19
Marcello Zamora <zamoramaj@gmail.com>
2021-01-12 16:28:09 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyDEXiwFZJLkf3kh8vmCyumrQatpzPDzJUk",
+    apiKey: "",
     authDomain: "yapper-37102.firebaseapp.com",
     databaseURL: "https://yapper-37102.firebaseio.com",
     projectId: "yapper-37102",
